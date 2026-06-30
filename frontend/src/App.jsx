66a3f03b6e2eeb3d508527870b8ffbbca89069d8
Ayush chaudahry <chaudahry.ayush2003@gmail.com>
2026-06-30 15:17:15 +0545

    Add pages and routes define

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+
 // Changed from "@base-ui/react" to your shadcn ui path
 import { Button } from "@/components/ui/button"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";import SignInForm from "./auth/forms/SignInForm";
+import SignUpForm from "./auth/forms/SignUpForm";
+import { Home } from "lucide-react";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import NewsArticles from "./pages/NewsArticles";
 
 const App = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button className="bg-red-500" variant="ghost">Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Auth Routes */}
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+
+        {/* Main Route */}
+        <Route path="/" element={<Home/>} /> 
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/news" element={<NewsArticles />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
